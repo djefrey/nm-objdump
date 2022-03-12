b8a68b915756de1f952d86361cda0143e0d800fd
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2022-03-12 15:51:03 +0100

    rewrite flags.h to remove illegal chars

--- include/flags.h
+++ include/flags.h
@@ -7,13 +7,13 @@
 
 #pragma once
 
-#define BFD_NO_FLAGS    0x00
-#define HAS_RELOC       0x01
-#define EXEC_P          0x02
-#define HAS_LINENO      0x04
-#define HAS_DEBUG       0x08
-#define HAS_SYMS        0x10
-#define HAS_LOCALS      0x20
-#define DYNAMIC         0x40
-#define WP_TEXT         0x80
-#define D_PAGED         0x100
+#define BFD_NO_FLAGS 0x00
+#define HAS_RELOC 0x001
+#define EXEC_P 0x002
+#define HAS_LINENO 0x004
+#define HAS_DEBUG 0x008
+#define HAS_SYMS 0x010
+#define HAS_LOCALS 0x020
+#define DYNAMIC 0x040
+#define WP_TEXT 0x080
+#define D_PAGED 0x100
